65c57a9f5d95ac05c22edb77b5906378084382e8
Kyle Yates <53956594+KyleTy1er@users.noreply.github.com>
2020-04-20 15:17:12 -0700

    added book routes

--- WEB_APP/__init__.py
+++ WEB_APP/__init__.py
@@ -2,15 +2,29 @@
 
 # web_app/__init__.py
 
- from flask import Flask
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 
 from web_app.routes.home_routes import home_routes
 from web_app.routes.book_routes import book_routes
+
+db = SQLAlchemy()
+
+migrate = Migrate()
 
 
 # application factory pattern
 def create_app():
     app = Flask(__name__)
+    # 3 slashes in sqlite:/// make this a relative path
+    app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///kyle_w_a.db"
+    # FOR MAC USE ABSOLUTE PATh
+    # app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:////Users/mjr/Desktop/web-app-inclass-11/web_app_12.db"
+
+    db.init_app(app)
+    migrate.init_app(app, db)
+
     app.register_blueprint(home_routes)
     app.register_blueprint(book_routes)
     return app
