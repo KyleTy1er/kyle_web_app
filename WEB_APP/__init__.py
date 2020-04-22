9f6c93598345e3cdd1c7ab7f0d73412b193664c0
Kyle Yates <53956594+KyleTy1er@users.noreply.github.com>
2020-04-22 09:20:20 -0700

    added db drop for resetting database

--- WEB_APP/__init__.py
+++ WEB_APP/__init__.py
@@ -5,6 +5,7 @@ from flask import Flask
 from web_app.models import db, migrate
 from web_app.routes.home_routes import home_routes
 from web_app.routes.twitter_routes import twitter_routes
+from web_app.routes.admin_routes import admin_routes
 
 def create_app():
     app = Flask(__name__)
@@ -16,6 +17,7 @@ def create_app():
 
     app.register_blueprint(home_routes)
     app.register_blueprint(twitter_routes)
+    app.register_blueprint(admin_routes)
 
     return app
 
